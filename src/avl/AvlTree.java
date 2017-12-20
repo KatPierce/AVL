733c43a77dba84c15d057e25aaf672b7fc27f358
Eugenia <eugeniyaryzanova@gmail.com>
2017-12-20 22:46:24 +0300

    исправила замечания

--- src/avl/AvlTree.java
+++ src/avl/AvlTree.java
@@ -11,168 +11,159 @@ import java.util.*;
  *
  * @author Eugenia
  */
-public class AvlTree<T1 extends Comparable<T1>,T2> implements Map<T1, T2> {
+public class AvlTree<T1 extends Comparable<T1>, T2> implements Map<T1, T2> {
+
     private Node<T1, T2> root;
     private int size = 0;
 
     public AvlTree() {
         root = null;
     }
-    
+
     private int height(Node<T1, T2> node) {
-        if (node == null)
+        if (node == null) {
             return -1;
+        }
         return node.height;
     }
+
     private int balanceCoef(Node<T1, T2> node) {
-        return (height(node.left)-height(node.right));        
+        return (height(node.left) - height(node.right));
     }
-    
-    private Node<T1, T2> rebalance (Node<T1, T2> node) {
+
+    private Node<T1, T2> rebalance(Node<T1, T2> node) {
         int coef = balanceCoef(node);
-        if (coef <-1) {
-            if (balanceCoef(node.right)>0)
+        if (coef < -1) {
+            if (balanceCoef(node.right) > 0) {
                 node.right = turnRight(node.right);
+            }
             node = turnLeft(node);
+        } else if (coef > 1) {
+            if (balanceCoef(node.left) < 0) {
+                node.left = turnLeft(node.left);
+            }
+            node = turnRight(node);
         }
-        else 
-            if (coef > 1) {
-                if (balanceCoef(node.left)<0) {
-                    node.left = turnLeft(node.left);
-                }
-                node = turnRight(node);
-            }
-        return node;    
-    }
-    
-    public void add( T1 key, T2 value) {
-        Data<T1,T2> data = new Data(key, value);
-        root = add(data,root);
-    }
-    
-    private Node<T1, T2> add(Data<T1,T2> data, Node<T1, T2> node) {
-        if (node == null)  {       
-            size++;
-            return new Node (data, 0);
-        }    
-        int comparison = ((T1)data.getKey()).compareTo((T1)node.data.getKey());
-        if (comparison > 0)
-            node.right = add(data, node.right);
-        else 
-            if (comparison <0) 
-                node.left = add(data, node.left);
-            else {
-                node.data.setValue(data.getValue());
-                return node;
-            }
-        node.height = 1+ Math.max(height(node.left), height(node.right));
-        node = rebalance(node);          
         return node;
     }
-    
-    
-    public T2 find(T1 key) {
-       T2 result = find(root, key);       
-       return result;
-    }
-    
-    private T2 find(Node<T1, T2> current, T1 key) {
-        T2 result = null;
-        int comparison = key.compareTo((T1)current.data.getKey());
-        if (comparison >0)
-            result = (T2)find(current.right, key);
-        else 
-            if (comparison <0)
-                result = (T2)find(current.left, key);
-            else 
-                result = (T2)current.data.getValue();
-        if (result == null) 
-            return null;
-        else 
-            return result;   
-            
-    }    
-  
-    
-    
-    public void remove( T1 key ) {
-        root = remove(key, root);
-    }
-    
-    private Node remove(T1 key, Node<T1, T2> node) {
-        if (node == null || find(key) == null)   
-            return node;         
-        int comparison = key.compareTo( (T1) node.data.getKey());
-        if (comparison < 0) {  
-            node.left = remove(key, node.left);
-        }
-        else
-            if (comparison > 0) { 
-            node.right = remove( key, node.right);
-        }
-        else { 
-            if (node.left == null) { 
-                return node.right;
-            }
-            else
-                if (node.right == null) { 
-                return node.left;
-                }
-                else {  
-                    Node<T1, T2> nodeToRemove = node;
-                    node = maxInSubtree(nodeToRemove.right);
-                    node.right = replaceMax(nodeToRemove.right);
-                    node.left = nodeToRemove.left;
 
-                }
+    public void add(T1 key, T2 value) {
+        Data<T1, T2> data = new Data(key, value);
+        root = add(data, root);
+    }
+
+    private Node<T1, T2> add(Data<T1, T2> data, Node<T1, T2> node) {
+        if (node == null) {
+            size++;
+            return new Node(data, 0);
+        }
+        int comparison = (data.getKey()).compareTo(node.data.getKey());
+        if (comparison > 0) {
+            node.right = add(data, node.right);
+        } else if (comparison < 0) {
+            node.left = add(data, node.left);
+        } else {
+            node.data.setValue(data.getValue());
+            return node;
         }
         node.height = 1 + Math.max(height(node.left), height(node.right));
-        return rebalance(node);    
-    }
-
-        private Node maxInSubtree( Node<T1, T2> node ) 
-    {
-        if( node == null )
-            return node;
-        while( node.right != null )
-            node = node.right;         
+        node = rebalance(node);
         return node;
     }
+
+    public T2 find(T1 key) {
+        T2 result = find(root, key);
+        return result;
+    }
+
+    private T2 find(Node<T1, T2> current, T1 key) {
+        T2 result = null;
+        int comparison = key.compareTo(current.data.getKey());
+        if (comparison > 0) {
+            result = (T2) find(current.right, key);
+        } else if (comparison < 0) {
+            result = (T2) find(current.left, key);
+        } else {
+            result = current.data.getValue();
+        }
+        if (result == null) {
+            return null;
+        } else {
+            return result;
+        }
+
+    }
+
+    public void remove(T1 key) {
+        root = remove(key, root);
+    }
+
+    private Node remove(T1 key, Node<T1, T2> node) {
+        if (node == null || find(key) == null) {
+            return node;
+        }
+        int comparison = key.compareTo(node.data.getKey());
+        if (comparison < 0) {
+            node.left = remove(key, node.left);
+        } else if (comparison > 0) {
+            node.right = remove(key, node.right);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            } else {
+                Node<T1, T2> nodeToRemove = node;
+                node = maxInSubtree(nodeToRemove.left);
+                node.left = replaceMax(nodeToRemove.left);
+                node.right = nodeToRemove.right;
+
+            }
+        }
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        return rebalance(node);
+    }
+
+    private Node maxInSubtree(Node<T1, T2> node) {
+        if (node == null) {
+            return node;
+        }
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node;
+    }
+
     private Node replaceMax(Node<T1, T2> node) {
-        if (node.right == null) 
+        if (node.right == null) {
             return node.left;
+        }
         node.right = replaceMax(node.right);
         node.height = 1 + Math.max(height(node.left), height(node.right));
         return rebalance(node);
-    }  
+    }
 
-    
     //////////////////////////////////////////////
-    
-    
-    
-   //повороты     
-    private Node<T1, T2> turnRight (Node<T1, T2> pivot) {        //pivot- узел,вокруг которого вращаем
+    //повороты     
+    private Node<T1, T2> turnRight(Node<T1, T2> pivot) {        //pivot- узел,вокруг которого вращаем
         Node<T1, T2> node = pivot.left;                 // node - кого вращаем
         pivot.left = node.right;
         node.right = pivot;
-        pivot.height = 1+ Math.max(height(pivot.left), height(pivot.right));
-        node.height = 1+ Math.max(height(node.left), height(node.right));
+        pivot.height = 1 + Math.max(height(pivot.left), height(pivot.right));
+        node.height = 1 + Math.max(height(node.left), height(node.right));
         return node;
     }
-    
-    private Node<T1, T2> turnLeft (Node<T1, T2> pivot) {        //pivot- узел,вокруг которого вращаем
+
+    private Node<T1, T2> turnLeft(Node<T1, T2> pivot) {        //pivot- узел,вокруг которого вращаем
         Node<T1, T2> node = pivot.right;
         pivot.right = node.left;
         node.left = pivot;
-        pivot.height = 1+ Math.max(height(pivot.left), height(pivot.right));
-        node.height = 1+ Math.max(height(node.left), height(node.right));
+        pivot.height = 1 + Math.max(height(pivot.left), height(pivot.right));
+        node.height = 1 + Math.max(height(node.left), height(node.right));
         return node;
     }
-    
+
 //////
-   
-  
     @Override
     public int size() {
         return size;
@@ -185,51 +176,56 @@ public class AvlTree<T1 extends Comparable<T1>,T2> implements Map<T1, T2> {
 
     @Override
     public boolean containsKey(Object key) {
-      if ((isEmpty() || find((T1)key) == null))
-          return false;
-      return true;
+        if ((isEmpty() || find((T1) key) == null)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean containsValue(Object value) {
-        if (isEmpty()) 
+        if (isEmpty()) {
             return false;
-        Set<T2> set = (Set)values();
-        if (set.contains(value))
+        }
+        Set<T2> set = (Set) values();
+        if (set.contains(value)) {
             return true;
+        }
         return false;
-        
+
     }
 
     @Override
     public T2 get(Object key) {
-        return find((T1)key);
+        return find((T1) key);
     }
 
     @Override
     public T2 put(T1 key, T2 value) {
-        if (containsKey(key)) {
-           T2 result = find(key); 
-           add(key,value);
-           return result;
+        if (find(key) != null) {
+            T2 result = find(key);
+            add(key, value);
+            return result;
         }
-        add(key,value);
-        return null;         
+        add(key, value);
+        return null;
     }
 
     @Override
     public T2 remove(Object key) {
-        if (containsKey(key)) {
-           T2 result = find((T1)key);            
-           remove(key);
-           return result;
-        }        
-        return null; 
+        if (find((T1) key) != null) {
+            T2 result = find((T1) key);
+            remove(key);
+            return result;
+        }
+        return null;
     }
 
     @Override
     public void putAll(Map<? extends T1, ? extends T2> m) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Map.Entry entry : m.entrySet()) {
+            put((T1) entry.getKey(), (T2) entry.getValue());
+        }
     }
 
     @Override
@@ -239,50 +235,73 @@ public class AvlTree<T1 extends Comparable<T1>,T2> implements Map<T1, T2> {
 
     @Override
     public Set<T1> keySet() {
-        Set <T1> result = null;
-       Iterator it = iterator();
-       while (it.hasNext()) {
-          Node<T1, T2> current = (Node)it.next();
-          result.add(current.data.getKey());          
-       }
-       return result;
+        Set<T1> result = null;
+        Iterator it = iterator();
+        while (it.hasNext()) {
+            Node<T1, T2> current = (Node) it.next();
+            result.add(current.data.getKey());
+        }
+        return result;
     }
 
     @Override
     public Collection<T2> values() {
-       Set <T2> result = null;
-       Iterator it = iterator();
-       while (it.hasNext()) {
-          Node<T1, T2> current = (Node)it.next();
-          result.add(current.data.getValue());          
-       }
-       return result;
+        Set<T2> result = null;
+        Iterator it = iterator();
+        while (it.hasNext()) {
+            Node<T1, T2> current = (Node) it.next();
+            result.add(current.data.getValue());
+        }
+        return result;
     }
 
     @Override
     public Set<Entry<T1, T2>> entrySet() {
-       Set <Entry<T1,T2>> result = null;
-       Iterator it = iterator();
-       while (it.hasNext()) {
-          Node<T1, T2> current = (Node)it.next();
-          result.add(current.data);          
-       }
-       return result;
+        Set<Entry<T1, T2>> result = null;
+        Iterator it = iterator();
+        while (it.hasNext()) {
+            Node<T1, T2> current = (Node) it.next();
+            result.add(current.data);
+        }
+        return result;
     }
-    
-//    //iterator 
-//    public class TreeIterator implements Iterator<Node<T1, T2>> {
-//        
-//    }
-//
-//       
-//    
-//    public Iterator<Node<T1,T2>> iterator() {
-//        return new TreeIterator();
-//    }
-    
 
+    //iterator 
+    public class TreeIterator implements Iterator<Node<T1, T2>> {
 
+        ArrayDeque<Node<T1, T2>> deque;
 
-     
+        public TreeIterator(Node<T1, T2> root) {
+            deque = new ArrayDeque<Node<T1, T2>>();
+            while (root != null) {
+                deque.push(root);
+                root = root.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !deque.isEmpty();
+        }
+
+        @Override
+        public Node<T1, T2> next() {
+            Node<T1, T2> result = deque.pop();
+
+            if (result.right != null) {
+                Node<T1, T2> node = result.right;
+                while (node != null) {
+                    deque.push(node);
+                    node = node.left;
+                }
+            }
+            return result;
+        }
+
+    }
+
+    public Iterator<Node<T1, T2>> iterator() {
+        return new TreeIterator(root);
+    }
+
 }
--- src/avl/Main.java
+++ src/avl/Main.java
@@ -9,9 +9,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-
-
-
 /**
  *
  * @author Eugenia
@@ -23,22 +20,22 @@ public class Main {
      */
     public static void main(String[] args) {
         AvlTree<Integer, String> tree = new AvlTree();
-       
+
         tree.add(7, "c");
-         tree.add(4, "a");
-        tree.add(3, "b");
-        tree.add(2, "c");
+        tree.add(4, "a");
+        tree.add(6, "b");
+        tree.add(3, "c");
         tree.add(5, "c");
-        tree.add(5, "g");
+        tree.add(9, "g");
 //        int a = tree.size();
 //        System.out.println(tree.size());
 //        tree.remove(7);
 //        tree.print2();
         Iterator it = tree.iterator();
-       while (it.hasNext()) {
-        System.out.println(it.next());
-       }     
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
 
     }
-    
+
 }
